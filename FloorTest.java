10955ead69136fa1ea573cd380603bf28f33a8d4
Pratik Gaurav <pratik.gaurav@oracle.com>
2024-04-30 20:17:56 +0530

    root commit

--- FloorTest.java
+++
@@ -1,8 +0,0 @@
-public class FloorTest{
-    public static int solve(int A) {
-        return (int)Math.floor(A/200);
-    }
-    public static void main(String... args){
-        System.out.println(solve(-771));;
-    }
-}
